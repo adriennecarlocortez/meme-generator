dbfce7ce63b41c80e491a3edb988a17ba665154c
adriennecarlocortez <cortez.adrienne00@gmail.com>
2023-11-19 19:08:43 +0800

    added coded by adrienne

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Meme from './components/Meme'
+import Copyright from './components/Copyright'
 
 function App() {
   const [count, setCount] = useState(0)
---
+++ src/components/Copyright.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Copyright() {
+    return (
+        <section className="copyright">
+			<small>Developed by Adrienne Carlo. All Rights Reserved.</small>
+		</section>
+    )
+}
+export default Copyright
